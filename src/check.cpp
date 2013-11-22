cbde98b1c495034f20e2de69e191771049310ae8
Cristi Cobzarenco <cristi.cobzarenco@gmail.com>
2013-11-22 03:10:31 +0000

    adds prettier stack trace

--- src/check.cpp
+++ src/check.cpp
@@ -16,23 +16,19 @@ namespace {
 //   http://stackoverflow.com/questions/3151779
 void __attribute__((noinline)) dump_trace() {
   void *trace[16];
-  char **messages = nullptr;
   int i, trace_size = 0;
   char name_buf[512];
   name_buf[readlink("/proc/self/exe", name_buf, 511)] = 0;
 
-  std::cerr << "Stack trace:" << std::endl;
-
   trace_size = backtrace(trace, 16);
-  messages = backtrace_symbols(trace, trace_size);
-
   for (i = 2; i < trace_size; ++i) {
-    std::cerr << "  #" << i - 1 << " " << messages[i] << std::endl << "       ";
+    std::cerr << (i == 2 ? "In " : "  called from ");
     std::cerr.flush();
 
     char syscom[256];
-    sprintf(syscom, "addr2line %p -e %s 1>&2", trace[i], name_buf);
-    system(syscom);
+    sprintf(syscom, "addr2line %p -spfCe %s 1>&2", trace[i], name_buf);
+    int ret = system(syscom);
+    if (ret) std::cerr << "<unknown>" << std::endl;
   }
 }
 
