8a39dfe298fcdcab8589406a04acc9c7c681d96e
Cristian Cobzarenco <cristi.cobzarenco@gmail.com>
2014-07-26 00:28:03 +0100

    allow thread_pool to use external ios

--- include/thread_pool.hpp
+++ include/thread_pool.hpp
@@ -11,10 +11,11 @@ namespace riak {
 
 class thread_pool {
  public:
-  static const size_t default_num_threads = 16;
+  static constexpr size_t use_hardware_threads = static_cast<size_t>(-1);
 
-  explicit thread_pool(size_t num_threads = default_num_threads);
-  virtual ~thread_pool() noexcept;
+  thread_pool(size_t num_threads = use_hardware_threads,
+              boost::asio::io_service* io_service = nullptr);
+  ~thread_pool() noexcept;
 
   boost::asio::io_service& io_service() noexcept { return io_service_; }
 
@@ -23,7 +24,8 @@ class thread_pool {
   }
 
  private:
-  boost::asio::io_service io_service_;
+  std::unique_ptr<boost::asio::io_service> io_service_ptr_;
+  boost::asio::io_service& io_service_;
   boost::asio::io_service::work work_;
   std::vector<std::thread> threads_;
 };
--- src/thread_pool.cpp
+++ src/thread_pool.cpp
@@ -4,17 +4,27 @@
 
 namespace riak {
 
-thread_pool::thread_pool(size_t num_threads) : work_{io_service_} {
+thread_pool::thread_pool(size_t num_threads,
+                         boost::asio::io_service* io_service)
+    : io_service_ptr_{io_service ? nullptr : new boost::asio::io_service{}},
+      io_service_(io_service_ptr_ ? *io_service_ptr_ : *io_service),
+      work_{io_service_} {
+  num_threads = num_threads == use_hardware_threads
+                    ? std::thread::hardware_concurrency()
+                    : num_threads;
   RIAKPP_CHECK_GT(num_threads, 0u);
   threads_.reserve(num_threads);
   for (size_t i = 0; i < num_threads; ++i) {
-    // TODO(cristicbz): Do somthing about exceptions?
     threads_.emplace_back([&] { io_service_.run(); });
   }
 }
 
 thread_pool::~thread_pool() noexcept {
-  io_service_.stop();
+  if (io_service_ptr_) {
+    io_service_.stop();
+  } else {
+    RIAKPP_CHECK(io_service_.stopped());
+  }
   for (auto& thread : threads_) thread.join();
 }
 
